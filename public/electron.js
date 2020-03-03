a974cc59710ea3b2cac21c725dfeb5758c4c87b0
MargVander <margauxvanderbeken@gmail.com>
2020-03-03 13:51:06 +0100

    first commit

---
+++ public/electron.js
@@ -0,0 +1,37 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
+const { autoUpdater } = require("electron-updater")
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', function() {
+    autoUpdater.checkForUpdatesAndNotify();
+    createWindow();
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+  autoUpdater.quitAndInstall();  
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
